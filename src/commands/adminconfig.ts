74828f6783569ec4c697c6db512d0597806e0d84
defnot001 <marc.miller994@gmail.com>
2024-03-06 00:00:06 +0100

    add config and adminconfig commands

---
+++ src/commands/adminconfig.ts
@@ -0,0 +1,178 @@
+import {
+  ApplicationCommandOptionType,
+  ChannelType,
+  Client,
+  GuildTextChannelType,
+  Snowflake,
+  TextChannel,
+  User,
+  escapeMarkdown,
+  inlineCode,
+  time,
+} from 'discord.js';
+import { Command } from '../handler/classes/Command';
+import { AdminModelController } from '../database/model/AdminModelController';
+import { config } from '../config';
+import Logger from '../log/logger';
+import {
+  ServerConfigModelController,
+  displayActionLevel,
+} from '../database/model/ServerConfigModelController';
+import { DbUser, UserModelController } from '../database/model/UserModelController';
+import { ScriptElementKindModifier, server } from 'typescript';
+import { pgClient } from '..';
+import { InfoEmbedBuilder } from '../builders';
+
+export default new Command({
+  name: 'adminconfig',
+  description: "Subcommands for admins to inspect the bot's server configs",
+  options: [
+    {
+      name: 'display',
+      description: 'Display the server configs for one or multiple servers',
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: 'server',
+          description:
+            'The ID(s) of the server(s) to display the config for. Separate multiple IDs with a comma (,). Max 5.',
+          type: ApplicationCommandOptionType.String,
+          required: true,
+        },
+      ],
+    },
+  ],
+  execute: async ({ interaction, args, client }) => {
+    await interaction.deferReply();
+
+    if (!(await AdminModelController.isAdmin(interaction.user.id))) {
+      await interaction.editReply('You do not have permission to use this command.');
+      Logger.warn(`${interaction.user.username} attempted to use /adminconfig without permission.`);
+      return;
+    }
+
+    if (!interaction.guild || interaction.guild.id !== config.adminServerID) {
+      await interaction.editReply('This command can only be used in the admin server.');
+      Logger.warn(
+        `${interaction.user.username} attempted to use /adminconfig outside of the admin server.`,
+      );
+      return;
+    }
+
+    const subcommand = args.getSubcommand() as 'display';
+
+    if (subcommand === 'display') {
+      const serverIDs = args
+        .getString('server', true)
+        .split(',')
+        .map((id) => id.trim());
+
+      if (!serverIDs.length) {
+        await interaction.editReply('You must provide at least one server ID.');
+        return;
+      }
+
+      if (serverIDs.length > 5) {
+        await interaction.editReply(
+          'You can only display the configs for up to 5 servers at a time.',
+        );
+        return;
+      }
+
+      try {
+        const serverConfigs = await ServerConfigModelController.getServerConfigs(serverIDs);
+
+        if (!serverConfigs.length) {
+          await interaction.editReply('No server configs found for the provided server IDs.');
+          return;
+        }
+
+        const embeds = await Promise.all(
+          serverConfigs.map(async (sc) => {
+            const guild = await client.guilds.fetch(sc.server_id);
+            const userIDs = await UserModelController.getUsersByServer(guild.id);
+            const users = await Promise.all(
+              userIDs.map(async (dbuser) => await client.users.fetch(dbuser.id)),
+            );
+            const logChannel = sc.log_channel
+              ? await getTextChannelByID(client, sc.log_channel)
+              : null;
+
+            const embed = new InfoEmbedBuilder(interaction.user, {
+              title: `Server Config for ${guild.name}`,
+              fields: [
+                {
+                  name: 'Server ID',
+                  value: inlineCode(guild.id),
+                },
+                {
+                  name: 'Whitelisted Admins',
+                  value: `${users.map((u) => `${escapeMarkdown(u.globalName ?? u.username)} (${inlineCode(u.id)})`).join('\n')}`,
+                },
+                {
+                  name: 'Log Channel',
+                  value: logChannel
+                    ? `${logChannel.name} (${inlineCode(logChannel.id)})`
+                    : 'Not set',
+                },
+                {
+                  name: 'Ping Admins',
+                  value: sc.ping_users ? 'Enabled' : 'Disabled',
+                },
+                {
+                  name: 'Action Level',
+                  value: displayActionLevel(sc.action_level),
+                },
+                {
+                  name: 'Timeout Users With Role',
+                  value: sc.timeout_users_with_role ? 'Enabled' : 'Disabled',
+                },
+                {
+                  name: 'Created At',
+                  value: `${time(new Date(sc.created_at), 'D')}\n(${time(new Date(sc.created_at), 'R')})`,
+                  inline: true,
+                },
+                {
+                  name: 'Updated At',
+                  value: `${time(new Date(sc.updated_at), 'D')}\n(${time(new Date(sc.updated_at), 'R')})`,
+                  inline: true,
+                },
+              ],
+            });
+
+            if (guild.iconURL()) {
+              embed.setThumbnail(guild.iconURL());
+            }
+
+            return embed;
+          }),
+        );
+
+        await interaction.editReply({ embeds });
+      } catch (e) {
+        await interaction.editReply('An error occurred while fetching the server configs.');
+        Logger.error(`An error occurred while fetching the server configs: ${e}`);
+        return;
+      }
+    }
+  },
+});
+
+export async function getTextChannelByID(
+  client: Client,
+  id: Snowflake,
+): Promise<TextChannel | null> {
+  try {
+    const channel = await client.channels.fetch(id);
+
+    if (channel && channel.isTextBased() && channel.type === ChannelType.GuildText) {
+      return channel as TextChannel;
+    }
+
+    Logger.warn(`Logchannel with ID ${id} is not a valid text channel.`);
+    return null;
+  } catch (e) {
+    Logger.error(`An error occurred while fetching channel with ID ${id}: ${e}`);
+    return null;
+  }
+}
---
+++ src/commands/config.ts
@@ -0,0 +1,231 @@
+import {
+  ApplicationCommandOptionType,
+  CommandInteraction,
+  Guild,
+  Interaction,
+  User,
+  inlineCode,
+  time,
+} from 'discord.js';
+import { Command } from '../handler/classes/Command';
+import { DbUser, UserModelController } from '../database/model/UserModelController';
+import { config } from '../config';
+import { InfoEmbedBuilder } from '../builders';
+import {
+  DbServerConfig,
+  ServerConfigModelController,
+  displayActionLevel,
+} from '../database/model/ServerConfigModelController';
+import { getTextChannelByID } from './adminconfig';
+
+export default new Command({
+  name: 'config',
+  description: 'Configure the bot for your server',
+  options: [
+    {
+      name: 'display',
+      description: 'Display the current configuration',
+      type: ApplicationCommandOptionType.Subcommand,
+    },
+    {
+      name: 'update',
+      description: 'Change the configuration of the bot',
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: 'logchannel',
+          description: 'The channel to log actions to',
+          type: ApplicationCommandOptionType.String,
+        },
+        {
+          name: 'pingusers',
+          description: 'Whether or not to ping users when action is taken',
+          type: ApplicationCommandOptionType.Boolean,
+        },
+        {
+          name: 'actionlevel',
+          description: 'The level of action to take',
+          type: ApplicationCommandOptionType.Integer,
+          choices: [
+            {
+              name: 'Notify',
+              value: 0,
+            },
+            {
+              name: 'Timeout',
+              value: 1,
+            },
+            {
+              name: 'Kick',
+              value: 2,
+            },
+            {
+              name: 'Soft Ban',
+              value: 3,
+            },
+            {
+              name: 'Ban',
+              value: 4,
+            },
+          ],
+        },
+        {
+          name: 'timeoutuserswithrole',
+          description: 'Whether or not to timeout users with a specific role',
+          type: ApplicationCommandOptionType.Boolean,
+        },
+      ],
+    },
+  ],
+  execute: async ({ interaction, args, client }) => {
+    await interaction.deferReply();
+
+    const interactionGuild = interaction.guild;
+
+    if (!interactionGuild) {
+      await interaction.editReply('This command can only be used in a server.');
+      return;
+    }
+
+    const dbUser = await isUserAllowed(interactionGuild, interaction);
+    if (!dbUser) return;
+
+    const subcommand = args.getSubcommand() as 'display' | 'update';
+
+    if (interactionGuild.id === config.adminServerID) {
+      await interaction.editReply('This command is not available in the admin server.');
+      return;
+    }
+
+    if (subcommand === 'display') {
+      try {
+        const serverConfig = await ServerConfigModelController.getServerConfig(interactionGuild.id);
+
+        if (!serverConfig) {
+          await interaction.editReply('Server config not found.');
+          return;
+        }
+
+        const embed = buildServerConfigEmbed({
+          guild: interactionGuild,
+          user: interaction.user,
+          serverConfig,
+        });
+
+        await interaction.editReply({ embeds: [embed] });
+      } catch (e) {
+        await interaction.editReply(`Failed to get server config: ${e}`);
+        return;
+      }
+    }
+
+    if (subcommand === 'update') {
+      const logChannel = args.getString('logchannel');
+      const pingUsers = args.getBoolean('pingusers');
+      const actionLevel = args.getInteger('actionlevel');
+      const timeoutUsersWithRole = args.getBoolean('timeoutuserswithrole');
+
+      try {
+        const serverConfig = await ServerConfigModelController.updateServerConfig({
+          server_id: interactionGuild.id,
+          log_channel: logChannel,
+          ping_users: pingUsers,
+          action_level: actionLevel,
+          timeout_users_with_role: timeoutUsersWithRole,
+        });
+
+        if (!serverConfig) {
+          await interaction.editReply('Server config not found.');
+          return;
+        }
+
+        const embed = buildServerConfigEmbed({
+          guild: interactionGuild,
+          user: interaction.user,
+          serverConfig,
+        });
+
+        await interaction.editReply({ content: 'Updated Serverconfig', embeds: [embed] });
+      } catch (e) {
+        await interaction.editReply(`Failed to update server config: ${e}`);
+        return;
+      }
+    }
+  },
+});
+
+async function isUserAllowed(
+  guild: Guild,
+  interaction: CommandInteraction,
+): Promise<DbUser | null> {
+  try {
+    const dbUser = await UserModelController.getUser(interaction.user.id);
+
+    if (!dbUser) {
+      await interaction.editReply('You are not allowed to use this command.');
+      return null;
+    }
+
+    if (!dbUser.servers.includes(guild.id) && guild.id !== config.adminServerID) {
+      await interaction.editReply('You are not allowed to use this command here.');
+      return null;
+    }
+
+    return dbUser;
+  } catch (e) {
+    await interaction.editReply(`Failed to get user: ${e}`);
+    return null;
+  }
+}
+
+function buildServerConfigEmbed(details: {
+  user: User;
+  guild: Guild;
+  serverConfig: DbServerConfig;
+}) {
+  const logChannel = details.serverConfig.log_channel
+    ? `<#${details.serverConfig.log_channel}>`
+    : 'Not set';
+
+  return new InfoEmbedBuilder(details.user, {
+    title: `Server Config for ${details.guild.name}`,
+    fields: [
+      {
+        name: 'Server ID',
+        value: inlineCode(details.guild.id),
+      },
+      {
+        name: 'Log Channel',
+        value: logChannel,
+      },
+      {
+        name: 'Ping Admins',
+        value: details.serverConfig.ping_users ? 'Enabled' : 'Disabled',
+      },
+      {
+        name: 'Action Level',
+        value: displayActionLevel(details.serverConfig.action_level),
+      },
+      {
+        name: 'Timeout Users With Role',
+        value: details.serverConfig.timeout_users_with_role ? 'Enabled' : 'Disabled',
+      },
+      {
+        name: 'Created At',
+        value: `${time(new Date(details.serverConfig.created_at), 'D')}\n(${time(
+          new Date(details.serverConfig.created_at),
+          'R',
+        )})`,
+        inline: true,
+      },
+      {
+        name: 'Updated At',
+        value: `${time(new Date(details.serverConfig.updated_at), 'D')}\n(${time(
+          new Date(details.serverConfig.updated_at),
+          'R',
+        )})`,
+        inline: true,
+      },
+    ],
+  });
+}
