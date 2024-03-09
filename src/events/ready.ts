54f6726b2e7856b1e66f709b59a4e9973adb9b76
defnot001 <marc.miller994@gmail.com>
2024-03-09 20:23:44 +0100

    set log channel when the bot is ready

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,18 +1,26 @@
-import { ActivityType, TextChannel } from 'discord.js';
+import { ActivityType, Client, TextChannel } from 'discord.js';
 import { Event } from '../handler/classes/Event';
-import { getTextChannelByID } from '../commands/adminconfig';
-import { botConfig } from '../config';
 import { LOGGER } from '../util/logger';
 // import { client } from '../index';
 // import { config } from '../config';
-
-export let errorLog: TextChannel | null = null;
 
 export default new Event('ready', async (c) => {
   c.user.setActivity('Bad Actors', { type: ActivityType.Watching });
   LOGGER.info(`Bot is ready! Logged in as ${c.user.username}.`);
 
-  errorLog = await getTextChannelByID(c, botConfig.adminServerErrorLogChannel);
-  LOGGER.info(`Error log channel set to ${errorLog?.name}!`);
+  const errorLog = await getErrorLogChannel(c);
+  await LOGGER.setLogChannel(errorLog);
+  LOGGER.info(`Set error log channel to ${errorLog.name} in ${errorLog.guild.name}.`);
+
   // await client.removeCommands();
 });
+
+async function getErrorLogChannel(client: Client) {
+  const channel = await client.channels.fetch(Bun.env.ADMIN_SERVER_ERROR_LOG_CHANNEL);
+
+  if (!channel || !channel.isTextBased() || !(channel instanceof TextChannel)) {
+    throw new Error('Error log channel not found.');
+  }
+
+  return channel;
+}
