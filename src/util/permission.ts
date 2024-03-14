e8dbd547423d71d896d549a330ad368686dc83a7
defnot001 <marc.miller994@gmail.com>
2024-03-14 19:38:08 +0100

    rename and add functions

--- src/util/permission.ts
+++ src/util/permission.ts
@@ -1,6 +1,6 @@
 import { Guild } from 'discord.js';
 import { AdminModelController } from '../database/model/AdminModelController';
-import { UserModelController } from '../database/model/UserModelController';
+import { DbUser, UserModelController } from '../database/model/UserModelController';
 import { ExtendedInteraction } from '../handler/types';
 import { LOGGER } from './logger';
 import { displayGuild, displayUser } from './discord';
@@ -8,17 +8,20 @@ import { botConfig } from '../config';
 
 /**
  * Checks if the user has permission to use the command by checking if the user exists in the users table in the database.
+ * Returns the user and guild if the user exists in the database.
  * Only works with deferred replies.
+ *
+ * **Sideeffect**: Sends a reply and logs a warning if the user does not exist in the database.
  */
-export async function hasUserPermission(options: {
+export async function checkUserInDatabase(options: {
   interaction: ExtendedInteraction;
   commandName: string;
-}): Promise<boolean> {
+}): Promise<{ dbUser: DbUser; guild: Guild } | null> {
   const { interaction, commandName } = options;
   const guild = await getInteractionGuild(interaction);
 
   if (!guild) {
-    return false;
+    return null;
   }
 
   try {
@@ -29,22 +32,24 @@ export async function hasUserPermission(options: {
         `User ${displayUser(interaction.user)} attempted to use /${commandName} in ${displayGuild(guild)} but the user does not exist in the database.`,
       );
       await interaction.editReply('You do not have permission to use this command.');
-      return false;
+      return null;
     }
 
-    return true;
+    return { dbUser, guild };
   } catch (e) {
     await LOGGER.error(`Error fetching user: ${e}`);
     await interaction.editReply('Error fetching user.');
-    return false;
+    return null;
   }
 }
 
 /**
  * Checks if the user has permission to use the command by checking if the user is an admin in the database.
  * Only works with deferred replies.
+ *
+ * **Sideeffect**: Sends a reply and logs a warning if the user is not an admin.
  */
-export async function hasAdminPermissions(options: {
+export async function checkAdminInDatabase(options: {
   interaction: ExtendedInteraction;
   commandName: string;
 }): Promise<boolean> {
@@ -75,6 +80,8 @@ export async function hasAdminPermissions(options: {
 /**
  * Checks if the interaction was created in the admin server.
  * Only works with deferred replies.
+ *
+ * **Sideeffect**: Sends a reply and logs a warning if the interaction was not created in the admin server.
  */
 export async function isInteractionInAdminServer(options: {
   interaction: ExtendedInteraction;
@@ -99,9 +106,36 @@ export async function isInteractionInAdminServer(options: {
 }
 
 /**
- * Checks if the command was used in a server and returns the guild if it exists.
+ * Checks if the interaction was created in one of the User's allowed servers.
+ * Only works with deferred replies.
+ *
+ * **Sideeffect**: Sends a reply and logs a warning if the interaction was not created in one of the User's allowed servers.
  */
-export async function getInteractionGuild(interaction: ExtendedInteraction): Promise<Guild | null> {
+export async function isInteractionInUsersAllowedServers(options: {
+  interaction: ExtendedInteraction;
+  commandName: string;
+  dbUser: DbUser;
+  guild: Guild;
+}): Promise<boolean> {
+  const { interaction, commandName, dbUser, guild } = options;
+
+  if (!dbUser.servers.includes(guild.id)) {
+    await LOGGER.warn(
+      `${displayUser(interaction.user)} attempted to use /${commandName} outside of their allowed server(s).`,
+    );
+    await interaction.editReply('This command can only be used your server(s).');
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * Checks if the command was used in a guild and returns the guild if it exists.
+ *
+ * **Sideeffect**: Sends a reply if the command was not used in a guild.
+ */
+async function getInteractionGuild(interaction: ExtendedInteraction): Promise<Guild | null> {
   if (!interaction.guild) {
     await interaction.editReply('This command can only be used in a server.');
     return null;
