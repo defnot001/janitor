d3dd96a7c3095fc62ffb0d7d230dda810e2ee5d8
defnot001 <marc.miller994@gmail.com>
2024-03-15 17:29:37 +0100

    add function to build server config embed

--- src/util/builders.ts
+++ src/util/builders.ts
@@ -1,5 +1,18 @@
-import { APIEmbed, ClientUser, EmbedBuilder, EmbedData, User } from 'discord.js';
+import {
+  APIEmbed,
+  ClientUser,
+  EmbedBuilder,
+  EmbedData,
+  Guild,
+  TextChannel,
+  User,
+  inlineCode,
+  time,
+} from 'discord.js';
 import { BroadcastType } from './broadcast';
+import { DbServerConfig, displayActionLevel } from '../database/model/ServerConfigModelController';
+import { displayUserFormatted } from './discord';
+import { ExtendedInteraction } from '../handler/types';
 
 export class InfoEmbedBuilder extends EmbedBuilder {
   constructor(user: User, data?: EmbedData | APIEmbed) {
@@ -47,4 +60,78 @@ function getBroadcastEmbedColor(broadcastType: BroadcastType) {
   }
 
   return 6_684_416; // green
+}
+
+export function buildServerConfigEmbed(options: {
+  interaction: ExtendedInteraction;
+  guild: Guild;
+  users: User[];
+  dbServerConfig: DbServerConfig;
+  logChannel: TextChannel | null;
+}) {
+  const { interaction, guild, users, dbServerConfig, logChannel } = options;
+
+  const embed = new InfoEmbedBuilder(interaction.user, {
+    title: `Server Config for ${guild.name}`,
+    fields: [
+      {
+        name: 'Server ID',
+        value: inlineCode(guild.id),
+      },
+      {
+        name: 'Whitelisted Admins',
+        value: users.map((user) => displayUserFormatted(user)).join('\n'),
+      },
+      {
+        name: 'Log Channel',
+        value: logChannel ? `${logChannel.name} (${inlineCode(logChannel.id)})` : 'Not set',
+      },
+      {
+        name: 'Ping Admins',
+        value: dbServerConfig.ping_users ? 'Enabled' : 'Disabled',
+      },
+      {
+        name: 'Ping Role',
+        value: dbServerConfig.ping_role ? `<@&${dbServerConfig.ping_role}>` : 'Not set',
+      },
+      {
+        name: 'Spam Action Level',
+        value: displayActionLevel(dbServerConfig.spam_action_level),
+      },
+      {
+        name: 'Impersonation Action Level',
+        value: displayActionLevel(dbServerConfig.impersonation_action_level),
+      },
+      {
+        name: 'Bigotry Action Level',
+        value: displayActionLevel(dbServerConfig.bigotry_action_level),
+      },
+      {
+        name: 'Timeout Users With Role',
+        value: dbServerConfig.timeout_users_with_role ? 'Enabled' : 'Disabled',
+      },
+      {
+        name: 'Ignored Roles',
+        value: dbServerConfig.ignored_roles.length
+          ? dbServerConfig.ignored_roles.map((role) => `<@&${role}>`).join(', ')
+          : 'None',
+      },
+      {
+        name: 'Created At',
+        value: `${time(new Date(dbServerConfig.created_at), 'D')}\n(${time(new Date(dbServerConfig.created_at), 'R')})`,
+        inline: true,
+      },
+      {
+        name: 'Updated At',
+        value: `${time(new Date(dbServerConfig.updated_at), 'D')}\n(${time(new Date(dbServerConfig.updated_at), 'R')})`,
+        inline: true,
+      },
+    ],
+  });
+
+  if (guild.iconURL()) {
+    embed.setThumbnail(guild.iconURL());
+  }
+
+  return embed;
 }
