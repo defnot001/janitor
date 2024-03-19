2c0bc5f5c190f2f4f78490e7846410dd60acfe1d
defnot001 <marc.miller994@gmail.com>
2024-03-19 14:21:14 +0100

    use static imports for commands instead of dynamic

--- src/handler/classes/Command.ts
+++ src/handler/classes/Command.ts
@@ -1,7 +1,30 @@
-import type { CommandOptions } from '../types';
+import type {
+	PermissionResolvable,
+	ChatInputApplicationCommandData,
+	CommandInteractionOptionResolver,
+	ApplicationCommandOptionData,
+} from 'discord.js';
+import type { ExtendedClient } from './ExtendedClient';
+import type { CommandOptions, ExtendedInteraction } from '../types';
 
-export class Command {
+export class Command implements ChatInputApplicationCommandData {
+	name: string;
+	description: string;
+	options?: readonly ApplicationCommandOptionData[];
+	defaultPermission?: boolean;
+	userPermissions?: PermissionResolvable;
+	execute: (options: {
+		client: ExtendedClient;
+		interaction: ExtendedInteraction;
+		args: CommandInteractionOptionResolver;
+	}) => Promise<void>;
+
 	constructor(commandOptions: CommandOptions) {
-		Object.assign(this, commandOptions);
+		this.name = commandOptions.name;
+		this.description = commandOptions.description;
+		this.options = commandOptions.options;
+		this.defaultPermission = commandOptions.defaultPermission;
+		this.userPermissions = commandOptions.userPermissions;
+		this.execute = commandOptions.execute;
 	}
 }
--- src/handler/classes/ExtendedClient.ts
+++ src/handler/classes/ExtendedClient.ts
@@ -1,23 +1,27 @@
-import { pathToFileURL } from 'bun';
-import {
-	type ApplicationCommandDataResolvable,
-	Client,
-	type ClientEvents,
-	Collection,
-} from 'discord.js';
-import { glob } from 'glob';
+import { type ApplicationCommandDataResolvable, Client, Collection } from 'discord.js';
 import { LOGGER } from '../../util/logger';
 import type { ClientStartOptions, CommandOptions, RegisterCommandOptions } from '../types';
-import type { Event } from './Event';
+import type { Command } from './Command';
+
+import { adminconfig } from '../../commands/adminconfig';
+import { adminlist } from '../../commands/adminlist';
+import { badactor } from '../../commands/badActor';
+import { config } from '../../commands/config';
+import { user } from '../../commands/user';
+
+import { interactionCreate } from '../../events/interactionCreate';
+import { ready } from '../../events/ready';
+
+export const COMMANDS: Command[] = [adminconfig, adminlist, badactor, config, user];
+export const EVENTS = [interactionCreate, ready];
 
 export class ExtendedClient extends Client {
 	public commands: Collection<string, CommandOptions> = new Collection();
 
 	public async start(options: ClientStartOptions) {
-		const { botToken, guildID, commandsPath, eventsPath, globalCommands, registerCommands } =
-			options;
+		const { botToken, guildID, globalCommands, registerCommands } = options;
 
-		await this.setModules(commandsPath, eventsPath);
+		await this.setModules();
 
 		if (registerCommands) {
 			const slashCommands: ApplicationCommandDataResolvable[] = this.commands.map(
@@ -91,32 +95,18 @@ export class ExtendedClient extends Client {
 		}
 	}
 
-	private async setModules(commandsPath: string, eventsPath: string) {
-		const commandPaths: string[] = await glob.glob(`${commandsPath.toString()}/*{.ts,.js}`);
-
-		for await (const path of commandPaths) {
-			const fileURL = pathToFileURL(path);
-			const command: CommandOptions = await this.importFile(fileURL.toString());
-
+	private async setModules() {
+		for (const command of COMMANDS) {
 			if (!command.name) {
-				throw new Error(`Command at path ${path} is missing the name property.`);
+				throw new Error('Command is missing the name property.');
 			}
 
 			this.commands.set(command.name, command);
 		}
 
-		const eventPaths: string[] = await glob.glob(`${eventsPath.toString()}/*{.ts,.js}`);
-
-		for await (const path of eventPaths) {
-			const fileURL = pathToFileURL(path);
-			const event: Event<keyof ClientEvents> = await this.importFile(fileURL.toString());
-
-			this.on(event.name, event.execute);
+		for (const event of EVENTS) {
+			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+			this.on(event.name, event.execute as any);
 		}
-	}
-
-	private async importFile(filePath: string) {
-		const file = await import(filePath);
-		return file.default;
 	}
 }
